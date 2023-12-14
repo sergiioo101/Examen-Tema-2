bc7bc04c474f4e41f59040237b1c3232a5353351
sergiioo101 <smartros@myuax.com>
2023-12-14 12:58:59 +0100

    Estructura asistencia

--- C++/main.cpp
+++ C++/main.cpp
@@ -4,3 +4,10 @@
 #include <algorithm>
 
 using namespace std;
+
+// Definición de la estructura Asistencia
+struct Asistencia {
+    string fecha;
+    string materia;
+    string estado; // Puede ser "asistió", "falta" o "tardanza"
+};
