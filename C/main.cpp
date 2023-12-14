3fe8d17f87765a1dafaeaa4bde3c07d0eba8b1d8
sergiioo101 <smartros@myuax.com>
2023-12-14 13:16:05 +0100

    funcion main

--- C/main.cpp
+++ C/main.cpp
@@ -82,3 +82,60 @@ void mostrarEstudiante(const struct Estudiante* estudiante) {
     mostrarMaterias(estudiante);
     mostrarAsistencias(estudiante);
 }
+
+int main() {
+    // Ejemplo de uso:
+    // Crear un objeto Estudiante
+    struct Estudiante estudiante1 = { "Juan", 20, 8.5, {}, 0 };
+
+    char opcion;
+
+    do {
+        // Mostrar menú
+        printf("\n1. Agregar materia\n");
+        printf("2. Eliminar materia\n");
+        printf("3. Registrar asistencia\n");
+        printf("4. Mostrar información del estudiante\n");
+        printf("0. Salir\n");
+        printf("Ingrese la opción: ");
+        scanf(" %c", &opcion);
+
+        switch (opcion) {
+            case '1': {
+                char nuevaMateria[50];
+                printf("Ingrese la nueva materia: ");
+                scanf("%s", nuevaMateria);
+                agregarMateria(&estudiante1, nuevaMateria);
+                break;
+            }
+            case '2': {
+                char materiaEliminar[50];
+                printf("Ingrese la materia a eliminar: ");
+                scanf("%s", materiaEliminar);
+                eliminarMateria(&estudiante1, materiaEliminar);
+                break;
+            }
+            case '3': {
+                char fecha[11], materia[50], estado[10];
+                printf("Ingrese la fecha de asistencia (formato YYYY-MM-DD): ");
+                scanf("%s", fecha);
+                printf("Ingrese la materia: ");
+                scanf("%s", materia);
+                printf("Ingrese el estado (asistió, falta, tardanza): ");
+                scanf("%s", estado);
+                registrarAsistencia(&estudiante1, fecha, materia, estado);
+                break;
+            }
+            case '4':
+                mostrarEstudiante(&estudiante1);
+                break;
+            case '0':
+                printf("Saliendo del programa.\n");
+                break;
+            default:
+                printf("Opción no válida. Intente de nuevo.\n");
+        }
+    } while (opcion != '0');
+
+    return 0;
+}
