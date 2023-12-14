afb4a5932b8ad4b3e82d967766c0766881bfbe6f
sergiioo101 <smartros@myuax.com>
2023-12-14 13:12:31 +0100

    estructura estudiante

--- C/main.cpp
+++ C/main.cpp
@@ -7,3 +7,12 @@ struct Asistencia {
     char materia[50];
     char estado[10]; // Puede ser "asistió", "falta" o "tardanza"
 };
+
+// Definición de la estructura Estudiante
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+    struct Asistencia asistencias[100]; // Array de asistencias del estudiante
+    int numAsistencias; // Número actual de asistencias
+};
