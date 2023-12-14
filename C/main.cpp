287423e1a4f36112484db48a0bcfbe56658f3659
sergiioo101 <smartros@myuax.com>
2023-12-14 13:12:11 +0100

    estructura asistencia

--- C/main.cpp
+++ C/main.cpp
@@ -1,4 +1,9 @@
 #include <cstdio>
 #include <cstring>
 
-
+// Definición de la estructura Asistencia
+struct Asistencia {
+    char fecha[11];
+    char materia[50];
+    char estado[10]; // Puede ser "asistió", "falta" o "tardanza"
+};
